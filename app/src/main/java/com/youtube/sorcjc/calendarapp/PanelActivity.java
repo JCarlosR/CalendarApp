b5b3fc091a1a63f5f7740717e76fce1bdf154cfc
Juan Carlos <juancagb.17@gmail.com>
2016-12-09 17:37:33 -0500

    Count down logic

--- app/src/main/java/com/youtube/sorcjc/calendarapp/PanelActivity.java
+++ app/src/main/java/com/youtube/sorcjc/calendarapp/PanelActivity.java
@@ -1,10 +1,16 @@
 package com.youtube.sorcjc.calendarapp;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class PanelActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +24,45 @@ public class PanelActivity extends AppCompatActivity implements View.OnClickList
 
         Button btnManageAccount = (Button) findViewById(R.id.btnManageAccount);
         btnManageAccount.setOnClickListener(this);
+
+        setupCountDown();
+    }
+
+    private void setupCountDown()
+    {
+        final TextView tvCountDown = (TextView) findViewById(R.id.tvRemainingCounter);
+        final String FORMAT = getString(R.string.remaining_counter);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2019);
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        Date targetDate = cal.getTime();
+
+        final long targetMillis = targetDate.getTime();
+        final long currentTimeMillis = System.currentTimeMillis();
+        final long remainingMillis = targetMillis - currentTimeMillis;
+
+        new CountDownTimer(remainingMillis, 1000) { // adjust the milli seconds here
+
+            public void onTick(long millisUntilFinished) {
+                final long days = TimeUnit.MILLISECONDS.toDays(millisUntilFinished);
+                final long hours = TimeUnit.MILLISECONDS.toHours(millisUntilFinished) - TimeUnit.DAYS.toHours(days);
+                final long minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished));
+                final long seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished));
+
+                tvCountDown.setText(String.format(FORMAT,
+                        days,
+                        hours,
+                        minutes,
+                        seconds
+                ));
+            }
+
+            public void onFinish() {
+                tvCountDown.setText("Mi momento ha llegado");
+            }
+        }.start();
     }
 
     @Override
